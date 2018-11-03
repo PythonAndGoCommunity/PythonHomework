22ad3d719bd1259792aaa6e04fa1c707ee281e41
Stacy <anastasyago@yandex.ru>
2018-11-03 21:31:59 +0300

    calc with comparison

--- calc.py
+++ calc.py
@@ -1,5 +1,5 @@
 import re
-
+import math
 
 binary_operations = {
     "+": 0,
@@ -15,16 +15,32 @@ binary_operations = {
 ops_list = dict(log=1, log10=1, abs=1, sqrt=1, sin=1, asin=1, cos=1, acos=1, hypot=1, tan=1, atan=1, atan2=1, ceil=1,
                 copysign=2, fabs=1, factorial=1, floor=1, fmod=2, frexp=1, ldexp=2, fsum=1, isfinite=1, isinf=1,
                 isnan=1, modf=1, trunc=1, exp=1, expm1=1, log1p=1, log2=1, pow=2, degrees=1, radians=1, cosh=1, sinh=1,
-                tanh=1, acosh=1, asinh=1, atanh=1, erf=1, erfc=1, gamma=1, lgamma=1, pi=1, e=1, inv=0, gcd=2, isclose=5,
-                isdexp=1, tau=1, inf=1, nan=1,
+                tanh=1, acosh=1, asinh=1, atanh=1, erf=1, erfc=1, gamma=1, lgamma=1, inv=0, gcd=2, isclose=5,
+                isdexp=1,
                 )
 
 
+constants = {
+    "pi",
+    "e",
+    "tau",
+    "inf",
+    "nan",
+}
 
+
+comparison_operators = {
+    "<",
+    "<=",
+    "==",
+    "!=",
+    ">=",
+    ">",
+}
 
 
 def correct_expression(expression):
-    re_expr = re.findall('log10|log2|log1p|expm1|atan2|\//|\d+\.\d+|\d+|\W|\w+',expression)
+    re_expr = re.findall('<=|==|!=|>=|log10|log2|log1p|expm1|atan2|\//|\d+\.\d+|\d+|\W|\w+',expression)
     i = 0
     if re_expr[0] == '-' and is_float(re_expr[1]):
         re_expr[1] *= -1
@@ -43,7 +59,7 @@ def correct_expression(expression):
             i += 1
             print('b')
         elif re_expr[i] == '(' and re_expr[i+1] == '-' and is_float(re_expr[i+2]):
-            re_expr[i+2] *= -1
+            re_expr[i+2] =re_expr[i+1] + re_expr[i+2]
             re_expr.pop(i+1)
 
         if i + 2 >= _len(re_expr):
@@ -51,8 +67,7 @@ def correct_expression(expression):
     return re_expr
 
 
-def ops_arg(expression):
-    print('ops:', id(expression))
+def get_arguments(expression):
     ops = expression.pop(0)
     res = []
     arg = []
@@ -84,13 +99,42 @@ def is_float(val):
     except ValueError:
         return False
 
+
 def calc(expression):
     stack = []
-    print('calc:', id(expression))
     while expression:
         i = expression[0]
         if is_float(i):
             stack.append(float(i))
+            expression.remove(i)
+        elif i in comparison_operators:
+            operator = expression.pop(0)
+            a = stack.pop()
+            b = calc(expression)
+            if operator == '<':
+                res = a < b
+            elif operator == '<=':
+                res = a <= b
+            elif operator == '==':
+                res = a == b
+            elif operator == '!=':
+                res = a != b
+            elif operator == '>=':
+                res = a >= b
+            elif operator == '>':
+                res = a>b
+            stack.append(res)
+        elif i in constants:
+            if i == "pi":
+                stack.append(math.pi)
+            elif i == "e":
+                stack.append(math.e)
+            elif i == "tau":
+                stack.append(math.tau)
+            elif i == "inf":
+                stack.append(math.inf)
+            elif i == "nan":
+                stack.append(math.nan)
             expression.remove(i)
         elif i in binary_operations:
             if len(stack)>1 and isinstance(stack[-1],(int,float)) and isinstance(stack[-2],(int,float)):
@@ -115,14 +159,14 @@ def calc(expression):
             expression.remove((i))
         if i in ops_list:
             arg = []
-            ops, arg0 = ops_arg(expression)
+            ops, arg0 = get_arguments(expression)
             while arg0:
                arg.append(calc(arg0.pop()))
             if ops == 'pow':
                 stack.append(pow(*arg))
             elif ops == 'abs':
                 stack.append(abs(*arg))
-        print(stack)
+    print(stack)
     return stack.pop()
 
 
@@ -131,9 +175,11 @@ def to_postfix(expression):
     stack = []
     ops_bracket = []
     for i in correct_expression(expression):
-        if is_float(i):
+        if is_float(i) or i in ops_list or i in constants:
             res.append(i)
-        elif i in ops_list:
+        elif i in comparison_operators:
+            while stack:
+                res.append(stack.pop())
             res.append(i)
         elif i == '(':
             if res[-1] in ops_list:
@@ -163,8 +209,9 @@ def to_postfix(expression):
     return res
 
  #a = calc(to_postfix('abs(2)+3*2'))
-#print('res', calc(to_postfix('abs(-2)+3*2')))
+print('res', calc(to_postfix('abs(-2)+3*2')))
+print(calc(to_postfix('pow(abs(3),abs(56-53)+1)+3*(3-4)+pi')))
 print(calc(to_postfix('pow(abs(2+1-1),abs(56-53)+1)+3*(3-4)')))
-#print(calc(to_postfix('pow(abs(2+1-1),abs(56-53)+1)+3*(3-4)')))
+print(calc(to_postfix("2+3-5+6<=6")))
 
 
