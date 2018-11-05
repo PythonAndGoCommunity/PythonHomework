1150f6d231bfff8352b415ec3024a91e983374aa
AntonCharnichenka <antt0n.chern1chenk0@gmail.com>
2018-11-06 01:12:45 +0300

    implementation8

--- final_task/setup.py
+++ final_task/setup.py
@@ -6,6 +6,6 @@ setup(
     author = 'Anton Charnichenka',
     author_email = 'antt0n.chern1chenk0@gmail.com',
     description = 'pure Python command line calculator',
-    packages = ['pycalc'],
+    packages = setuptools.find_packages(),
     entry_points = {'console_scripts': ['pycalc = pycalc.__main__:main']},
     classifiers=["Programming Language :: Python :: 3.6", "Operating System :: Linux Mint"])
