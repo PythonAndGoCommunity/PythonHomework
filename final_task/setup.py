a73f68cb530939ac8d828ed040cbbf1e678a6c2d
Vladislav Pirtan <Vpirtan@yandex.ru>
2018-11-07 20:36:28 +0300

    setup.py fix

--- final_task/setup.py
+++ final_task/setup.py
@@ -1,10 +1,10 @@
-from setuptools import setup, find_packages	
+from setuptools import setup, find_packages
 
 setup(
-	name='pycalc',
-	version='0.1',
-	author='Vladislav Pirtan',
-	author_email='Vpirtan@yandex.ru',
-	packages=find_packages(),
-	entry_points={'console_scripts': ['pycalc=pycalc.__main__:main']},
+    name='pycalc',
+    version='0.1',
+    author='Vladislav Pirtan',
+    author_email='Vpirtan@yandex.ru',
+    packages=find_packages(),
+    entry_points={'console_scripts': ['pycalc=pycalc.__main__:main']},
 )
