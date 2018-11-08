85ea04bab1f851dff77661adee0b71a559cea026
Alexander Gutyra <gutyra13@gmail.com>
2018-11-08 23:43:49 +0300

    Fixed setup.py

--- final_task/setup.py
+++ final_task/setup.py
@@ -4,9 +4,10 @@ setup(
     name="Pycalc project",
     version='1.0',
     author="Alexander Gutyra",
-    author_email="gutyra13@gmail.com"
-    description="Simple pure-Python calculator with custom modules support."
+    author_email="gutyra13@gmail.com",
+    description="Simple pure-Python calculator with custom modules support.",
     packages=find_packages(),
     entry_points={
         'console_scripts': ['pycalc = pycalc.__main__:main']
     }
+)
