268c179d07af0ebf926fd34aef08215f97f8ec51
VladaGaravaya <36517813+VladaGaravaya@users.noreply.github.com>
2018-11-12 22:58:39 +0300

    change pycodestyle

--- final_task/pycalc/pycalc.py
+++ final_task/pycalc/pycalc.py
@@ -6,7 +6,7 @@ OPERATORS = {'+': operator.add,
              '*': operator.mul,
              '/': operator.truediv,
              '^': operator.pow,
-             '%': operator.mod }
+             '%': operator.mod}
 
 FUNCTIONS = ['abs',
              'sin',
@@ -14,36 +14,16 @@ FUNCTIONS = ['abs',
              'log10',
              'log']
 
-
-def searchFunc(expr):
-    if symbol in FUNCTIONS:
-        start = expr.find(symbol)
-        if start != -1:
-            start = start+len(symbol)+1
-            newExpr = expr[start:]
-            bracketsAmount=0
-            for i in newExpr:
-                if i == '(':         
-                    bracketsAmount+=1
-                elif i == ')' and bracketsAmount != 0:       
-                    bracketsAmount-=1
-                elif i == ')':
-                    break
-                count+=1
-            newExpr = newExpr[:count]
-            searchFunc(newExpr)
-        else 
-
 def checkBrackets(expression):
-    bracketsAmount=0           
-    for symbol in expression:   
-        if bracketsAmount==-1:  
+    bracketsAmount = 0           
+    for symbol in expression:
+        if bracketsAmount == -1:  
             return False        
         if symbol == '(':         
-            bracketsAmount+=1
+            bracketsAmount += 1
         elif symbol == ')':       
-            bracketsAmount-=1
-    if bracketsAmount!=0:       
+            bracketsAmount -= 1
+    if bracketsAmount != 0:       
         return False            
     return True
 
@@ -59,32 +39,32 @@ def convertExpr(expr):
             out += symbol
         elif symbol in allowedNums():
             out += symbol
-            iter+=1
+            iter += 1
         elif symbol in OPERATORS:
             out += ' %s ' % (symbol)
-            iter+=1
+            iter += 1
         if symbol == ')' and len(expr) == i+1:
             out += ' )'
-            iter=0
+            iter = 0
         elif symbol == ')' and expr[i+1] in allowedNums():
             out += ' ) * '
-            iter+=1
+            iter += 1
         elif symbol == ')' :
             out += ' )'
-            iter+=1
+            iter += 1
         if symbol == '(' and iter == 0:
             out += '( '
-            iter=0
+            iter = 0
         elif symbol == '(' and expr[i-1] in allowedNums():
             out += ' * ( '
-            iter=0
+            iter = 0
         elif symbol == '(' and expr[i-1] == ')':
             out += ' * ( '
-            iter=0
+            iter = 0
         elif symbol == '(':
             out += '( '
-            iter=0
-        i+=1
+            iter = 0
+        i += 1
     return out.strip()
             
 
