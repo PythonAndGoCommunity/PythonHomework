38f3e905756f84452c3d42cfb0a1d610f71d1866
Stacy <anastasyago@yandex.ru>
2018-11-13 22:52:21 +0300

    Add use-modules support

--- final_task/pycalc/calc.py
+++ final_task/pycalc/calc.py
@@ -2,6 +2,7 @@
 import re
 import math
 import argparse
+import importlib
 
 
 class CalcError(Exception):
@@ -81,7 +82,7 @@ def insert(regex, expression, token):
 
 
 def fix_missing_zero(expression):
-    """Inserts a zero in the number of the construction .number: .3 -> 0.3
+    """Inserts a zero in the number of the construction .number: .3 => 0.3
     Args:
         expression: input string with math expression
     Returns:
@@ -98,13 +99,12 @@ def fix_missing_zero(expression):
 
 
 def match_negative_value(expression):
-    """Fix missing -1, if math function looks like -sin
+    """change unary minus to '~' for processing
     Args:
         expression: input string with math expression
     Returns:
         The return string with correct negative value
     """
-    token = '1'
     regex = re.compile(r'(?<=^-)(?=[a-z])|(?<=\(-)(?=[a-z])|(?<=\^\-)(?=[a-z])|(?<=\*\-)(?=[a-z])|'
                        r'(?<=\/\-)(?=[a-z])|(?<=\s\-)(?=[a-z])')
     find = re.search(regex, expression)
@@ -143,15 +143,12 @@ def correct_expression(expression):
         The return list of tokens
     """
     res = []
-    if '()' in expression:
-        raise CalcError('ERROR: invalid bracket expression')
-    elif expression == '':
+    if expression == '':
         raise CalcError('ERROR: empty expression')
     expression = insert_multiplication(match_negative_value(fix_missing_zero(fix_multi_operations(expression))))
     regex = re.compile(r'(<=|==|!=|>=|(?<=[^a-z])e|e|pi|tau|inf|nan|log1p|^-\d+\.\d+|^-\d+|(?<=\W\W)\-\d+\.\d+|'
-                       r'(?<=\W\W)\-\d+|'
-                       r'(?<=\()\-\d+\.\d+|(?<=\()\-\d+|(?<=[a-z]\W)\-\d+\.\d+|(?<=[a-z]\W)\-\d+|(?<=\))\-|'
-                       r'\//|\/|\d+\.\d+|\d+|\W|\w+)')
+                       r'(?<=\W\W)\-\d+|(?<=\()\-\d+\.\d+|(?<=\()\-\d+|(?<=[a-z]\W)\-\d+\.\d+|(?<=[a-z]\W)\-\d+|'
+                       r'(?<=\))\-|\//|\/|\d+\.\d+|\d+|\W|\w+)')
     re_expr = re.split(regex, expression)
     re_expr = [x for x in re_expr if x and x != ' ']
     for i in reversed(range(len(re_expr))):
@@ -212,10 +209,24 @@ def is_float(value):
         return False
 
 
-def calc_iteration(expression):
+def is_func(value):
+    """Check is value function
+    Args:
+        value: expression token
+    Returns:
+        The return True, if value is function and False if not
+    """
+    if value[0].isalpha or value[0] == '_':
+        return True
+    else:
+        return False
+
+
+def calc_iteration(expression, mod_list):
     """Calculate math expression
     Args:
         expression: input string with math expression
+        mod_list: list of module names
     Returns:
         The return result of calculation
     """
@@ -282,13 +293,16 @@ def calc_iteration(expression):
             else:
                 stack.append(i)
             expression.remove(i)
-        if i in ops_list:
+        else:
             arg = []
             ops, arg0 = get_arguments(expression)
-            while arg0:
-                arg.append(calc_iteration(arg0.pop(0)))
+            while arg0 and arg0 != [[]]:
+                arg.append(calc_iteration(arg0.pop(0), mod_list))
             try:
-                if ops == 'round':
+                token = get_func(mod_list, ops)
+                if token:
+                    stack.append(token(*arg))
+                elif ops == 'round':
                     stack.append(round(*arg) * inv)
                 elif ops == 'abs':
                     stack.append(abs(*arg) * inv)
@@ -319,16 +333,14 @@ def to_postfix(expression):
         raise CalcError('ERROR: invalid operator "{0}"'.format(expression[0]))
     for item in range(len(expression)):
         i = expression[item]
-        if is_float(i) or i in ops_list or i in constants or i in unary_operation:
-            res.append(i)
-        elif i in comparison_operators:
+        if i in comparison_operators:
             while stack:
                 res.append(stack.pop())
             res.append(i)
         elif i == '(':
             if item + 1 >= len(expression) or expression[item + 1] in binary_operations:
                 raise CalcError('ERROR: invalid operator')
-            if res and res[-1] in ops_list:
+            if res and is_func(res[-1]):
                 ops_bracket.append(i)
             stack.append(i)
         elif i == ')':
@@ -353,6 +365,8 @@ def to_postfix(expression):
             while stack[-1] != '(':
                 res.append(stack.pop())
             res.append(i)
+        elif is_float(i) or is_func(i) or i in unary_operation:
+              res.append(i)
         else:
             raise CalcError('ERROR: input invalid token "{0}"'.format(i))
     for i in reversed(stack):
@@ -362,26 +376,44 @@ def to_postfix(expression):
     return res
 
 
-def evaluate(expression):
+def evaluate(expression, mod_list):
     """Evaluate expression
     Args:
         expression: input string with math expression
+        mod_list: list of module names
     Returns:
         The return result of evaluate
     """
-    return calc_iteration(to_postfix(expression))
+    return calc_iteration(to_postfix(expression), mod_list)
+
+
+def get_func(module_list, func_name):
+    """Find function in imported module
+    Args:
+        module_list: list of modules to import
+        func_name: function name
+    Returns:
+        The return function
+    """
+    for mod in module_list:
+        imported = importlib.import_module(mod)
+        if hasattr(imported, func_name):
+            return getattr(imported, func_name)
 
 
 def main():
     """Calc main function"""
     parser = argparse.ArgumentParser(description='Pure-python command-line calculator.')
+    parser.add_argument("-m", "--use-modules", dest='MODULE', nargs='+',
+                        required=False, default='', help="additional modules to use")
     parser.add_argument("EXPRESSION", help="expression string to evaluate")
     args = parser.parse_args()
     try:
-        print(evaluate(args.EXPRESSION))
+        print(evaluate(args.EXPRESSION, args.MODULE))
     except CalcError as exception:
         print(exception)
 
 
 if __name__ == '__main__':
     main()
+
