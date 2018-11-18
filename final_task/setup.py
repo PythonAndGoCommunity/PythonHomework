0ca1dd42f349a0d57cda57878d9b939246c742b0
Korotynski Alexey <korotynski.alexey@gmail.com>
2018-11-18 23:33:12 +0300

    change version

--- final_task/setup.py
+++ final_task/setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pycalc',
-    version='1.0',
+    version='1.1',
     packages=find_packages(),
     py_modules=['main'],
     entry_points={
