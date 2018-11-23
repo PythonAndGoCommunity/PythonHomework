a4a106d9b08396661e84424bc4e9a4b0ea55867f
Mikhailov Anton <delta.99@mail.ru>
2018-11-23 17:21:28 +0300

    Fixed comparison bug.

--- final_task/pycalc/pycalc.py
+++ final_task/pycalc/pycalc.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Main module, that takes the command line argument and sends it on its way to verify and calculate."""
 import sys
+import operator
 import pycalc.parse as parse
 import pycalc.compexp as compexp
 import pycalc.custom_exception as custom_exc
@@ -22,11 +23,13 @@ def main():
             answer = calcexp.calculate_expression(rpn_expression)
             print(answer)
         else:
+            if len(comp_checked[0].replace(" ", "")) == 0 or len(comp_checked[1].replace(" ", "")) == 0:
+                raise custom_exc.VerifyException("""ERROR: one side of inequality is empty.""")
             rpn_expression_left = expproc.verify_expression(comp_checked[0])
             answer_left = calcexp.calculate_expression(rpn_expression_left)
             rpn_expression_right = expproc.verify_expression(comp_checked[1])
             answer_right = calcexp.calculate_expression(rpn_expression_right)
-            return comp_checked[2](answer_left, answer_right)
+            print(comp_checked[2](answer_left, answer_right))
     except (custom_exc.VerifyException, ValueError, ZeroDivisionError) as error:
         print(error.msg)
         sys.exit(1)
