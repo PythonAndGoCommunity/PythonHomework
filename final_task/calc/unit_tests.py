e0325f14c8296c2e5d8de79a7c85f35a84936e84
Efim Alekseevich <efimprostopro@gmail.com>
2018-12-05 01:01:36 +0300

    add some unittests

--- final_task/calc/unit_tests.py
+++ final_task/calc/unit_tests.py
@@ -1,6 +1,7 @@
 import unittest
 
 from calc.other_functions import *
+from calc.math_functions import decide_func
 
 
 class TestFindingElements(unittest.TestCase):
@@ -77,3 +78,53 @@ class TestGetArgs(unittest.TestCase):
 
         formated_expression = get_args(['(', 'log', '(', 2, ',', 2, ')', ',', 4, ')'])
         self.assertEqual(formated_expression, [['log', '(', 2, ',', 2, ')'], [4]])
+
+
+class TestMiniFunctions1(unittest.TestCase):
+    """Test for minifunction from other_functions.py"""
+
+    def test15_vn(self):
+        """Function 'verify_num' verify performance"""
+
+        formated_expression = verify_num('12.3456')
+        self.assertEqual(formated_expression, 12.3456)
+
+    def test16_pe1(self):
+        """Function 'verify_pi_e' verify performance"""
+
+        formated_expression = verify_pi_e('pi', [])
+        self.assertEqual(formated_expression, '')
+
+    def test17_pe2(self):
+        """Function 'verify_pi_e' verify performance"""
+
+        formated_expression = verify_pi_e('pow', [])
+        self.assertEqual(formated_expression, 'pow')
+
+    def test18_pr1(self):
+        """Function 'prior' verify performance"""
+
+        formated_expression = prior(12.34)
+        self.assertEqual(formated_expression, 5)
+
+    def test19_pr2(self):
+        """Function 'prior' verify performance"""
+
+        formated_expression = prior('log')
+        self.assertEqual(formated_expression, 5)
+
+    def test20_bo(self):
+        """Function 'bin_operate' verify performance"""
+
+        formated_expression = bin_operate(2, 2, '^')
+        self.assertEqual(formated_expression, 4)
+
+
+class TestDecideFunction(unittest.TestCase):
+    """Test for function 'decide_function' from math_functions.py"""
+
+    def test21_mdf(self):
+        """verify performance"""
+
+        formated_expression = decide_func('pow', [2, 2])
+        self.assertEqual(formated_expression, 4)
