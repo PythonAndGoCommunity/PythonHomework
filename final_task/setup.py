436071c3671e5b125ebd42aad6e59518faf88f90
Gleb <nikitin_gleb@tut.by>
2018-12-08 23:21:40 +0300

    Pycalc release v1.0.7.

--- final_task/setup.py
+++ final_task/setup.py
@@ -1,12 +1,10 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
     name='pycalc',
-    version='1.0.2.',
-    packages=[''],
-    url='',
-    license='Free',
+    version='1.0.0',
     author='Gleb Nikitin',
-    author_email='nikitin_gleb@tut.by',
-    description='Python calculator.'
+    author_email='nikitin_gleb@tut.byu',
+    packages=find_packages(),
+    entry_points={'console_scripts': ['pycalc=pycalc.pycalc:main']},
 )
