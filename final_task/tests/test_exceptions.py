e51914f6c3c5308a6f46da6fb9d15c46f5764f48
Roma <roma@ssezmor-macbook.local>
2018-12-17 02:36:42 +0300

    Add unittest for module exception.

---
+++ final_task/tests/test_exceptions.py
@@ -0,0 +1,19 @@
+"""Unittest for module exeptions."""
+
+import unittest
+
+from pycalc_src.exceptions import BaseCalculatorException
+from pycalc_src.exceptions import CalculatorError
+
+class TestStringMethods(unittest.TestCase):
+    """Docstring."""
+
+    def test_base_calculator_exeption__valid_expressions(self):
+        """Docstring."""
+
+        with self.assertRaises(BaseCalculatorException):
+            raise CalculatorError(None, 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
