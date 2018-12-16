4ed2fc7d695f21bbc540dab81091b78a65df25d8
Kaleko Nikita, 650501 <36746354+Shalynishka@users.noreply.github.com>
2018-12-16 22:39:49 +0300

    Update pycalc.py
    Style code updated.

--- final_task/pycalc/pycalc.py
+++ final_task/pycalc/pycalc.py
@@ -7,32 +7,31 @@ from sys import path
 
 class Calc:
     """
-    Pretty class for cmd evaluating
+    Class for cmd evaluating. Contains methods and Attributes for converting a mathematical expression to Reverse
+    Polish Entry and its calculation.
     Attributes:
         const -> dict - contains math constant values
         func -> dict - contains tuples from math funcs and priority 4
         bin_op -> dict - contains tuples from binary funcs and their priority
         cmp -> dict - contains compare operations
-        users_mod -> dict - users modules
+        users_mod -> dict - user modules
 
         eval_list -> list - contains reverse polish entry
         op_st -> list - operations stack
         cmp_op -> list - contains compare operations from Expression to evaluate
-        
+
         implicit_mul -> bool - flag of implicit multiplication
         
         lim -> int - length limit of operation names
     Methods:
-        __init__(self, users) - collects member names of user modules 
-        my_eval(self, st) - evaluates Expression and return value or bool
-        make_note(self, st, impl=False) - makes reverse polish entry and write it to eval_list
-        eval_not(self) - evaluates RPE using eval_list as source
-        check_num(self, temp_num) - converts str to int or float and store num in eval_list
+        evaluate_expression(self, st) - evaluate Expression and return value or bool
+        make_note(self, st, impl=False) - make reverse polish entry and write it to eval_list
+        eval_not(self) - evaluate RPE using eval_list as source
     """
-    const = {'pi': math.pi, 'e': math.e, 'q': math.pi * math.e}
+    const = {attr: getattr(math, attr) for attr in dir(math) if '_' not in attr and not callable(getattr(math, attr))}
     func = {'round': (round, 4), 'abs': (abs, 4)}
 
-    users_mod = {}
+    user_modules = {}
     bin_op = {'+': (operator.add, 1), '-': (operator.sub, 1), '*': (operator.mul, 2),
               '/': (operator.truediv, 2), '%': (operator.mod,  2), '$': (operator.floordiv, 2), '^': (operator.pow, 3)}
   
@@ -48,94 +47,111 @@ class Calc:
     # лимит на длину буквенного выражения. Если его превысить -> raise ValueError
     lim = max([len(i) for i in dir(math) if '_' not in i])
 
-    cmp_op = []
+    # список операторов сравнения, если такие будут в выражении
+    cmp_in_expr = []
 
-    def __init__(self, users: list):
-        for u in [u + '.py' for u in users if '.py' not in u]:
-            for p in path:
-                try:
-                    spec = importlib.util.spec_from_file_location('', location=p + r'\\' + u)
-                    foo = importlib.util.module_from_spec(spec)
-                    spec.loader.exec_module(foo)
-                except:
-                    continue
-                else:
-                    self.users_mod[foo] = dir(foo)
+    def __init__(self, modules):
+        if modules:
+            for mod in [mod + '.py' for mod in modules if '.py' not in mod]:
+                for p in path:
+                    try:
+                        spec = importlib.util.spec_from_file_location('', location=p + r'\\' + mod)
+                        foo = importlib.util.module_from_spec(spec)
+                        spec.loader.exec_module(foo)
+                    except:
+                        raise Exception('module {} not found or cannot be loaded!'.format(mod))
+                    else:
+                        self.user_modules[foo] = dir(foo)
 
-    def my_eval(self, st: str):
-        for k in self.cmp.keys():
-            if k in st:
-                self.cmp_op.append(self.cmp[k])
-                st = st.replace(k, ' ')
-        if not self.cmp_op:
-            self.make_note(st)          # сборка ОПЗ
-            return self.eval_note()   # вычисление ОПЗ
+    def evaluate_expression(self, expr):
+        """
+        Gets string with math expression and returns result of the expression.
+        The definition of comparison occurs in this function. In this case, expression will be divided into parts.
+        Result of evaluating of this parts will be compared.
+        :param expr: str
+        :return: int, float or bool value
+        """
+        for key in self.cmp.keys():
+            if key in expr:
+                self.cmp_in_expr.append(self.cmp[key])
+                expr = expr.replace(key, ' ')
+        if not self.cmp_in_expr:
+            self.make_note(expr)          # сборка ОПЗ
+            return self.eval_note()       # вычисление ОПЗ
         else:
-            st = st.split(' ')
+            expr_list = expr.split(' ')   # разбиение строки выражения на подстроки
             res = []
-            for string in st:
+            for string in expr_list:
                 self.make_note(string)              # сборка ОПЗ
-                res.insert(0, self.eval_note())   # вычисление ОПЗ
+                res.insert(0, self.eval_note())     # вычисление ОПЗ
                 self.eval_list = []
-            for op in self.cmp_op:
-                if not op(res[1], res[0]):
+            for cmp_operator in self.cmp_in_expr:
+                if not cmp_operator(res[1], res[0]):
                     return False
                 del res[0]
             return True
 
-    def make_note(self, st: str, impl=False):
+    def make_note(self, expr, impl=False):
+        """
+        Gets string with math expression(without compare) and stores Reverse Polish Entry in self.eval_list.
+        Optional parameter 'impl' used to handle implicit multiplications using recursion.
+        :param expr: str
+        :param impl: bool
+        :return None
+        """
         self.unary = True
         self.implicit_mul = False
-        egg = ''       # содержит операцию
-        temp_num = ''  # содержит операнд
-        for c in st:
-            if c.isdigit() or c == '.':         # формируем строку с числом если текущий элемент - цифра
+        temp_operation = ''       # содержит не числовое выражение
+        temp_num = ''             # содержит числовое выражение
+        for char in expr:
+            if char.isdigit() or char == '.':         # формируем строку с числом если текущий элемент - цифра
                 if self.implicit_mul is True:   # между цифрой и предыдущем элементом есть неявное умножение
                     self.make_note('*', True)
-                temp_num += c
+                temp_num += char
             else:                               # текущий элемент не цифра
                 if temp_num:                    # заносим сформированное число в выходной лист ОПЗ
                     self.unary = False
                     self.check_num(temp_num)
                     temp_num = ''
                     # после числа неявное умножение
-                    if c not in self.bin_op.keys() and c != ')' and c != ',' and c != '.':
+                    if char not in self.bin_op.keys() and char != ')' and char != ',' and char != '.':
                             self.make_note('*', True)
                 else:
                     if self.unary:
-                        if c == '-':
+                        if char == '-':
                             self.op_st.insert(0, (operator.neg, 4))
-                            egg = ''
+                            temp_operation = ''
                             continue
-                        elif c == '+':
+                        elif char == '+':
                             self.op_st.insert(0, (operator.pos, 4))
-                            egg = ''
+                            temp_operation = ''
                             continue
 
-                if c == ',':    # функция round через ',' может получить второй параметр
+                if char == ',':    # функция round через ',' может получить второй параметр
                     continue
-
-                egg += c  # формируем строку с буквами - sin/pi/epi и тд. Из этого потом сформируем функции или const
+                    
+                # формируем строку с буквами - sin/pi/epi и тд. Из этого потом сформируем функции или const
+                temp_operation += char
 
                 # попытка вытащить текущий egg из пользовательского модуля
-                for u, d in self.users_mod.items():
-                    if egg in d:                                # получилось вытащить
-                        temp = getattr(u, egg)
-                        if callable(temp):                      # функция
+                for module_name, module_attributes in self.user_modules.items():
+                    if temp_operation in module_attributes:                 # получилось вытащить
+                        temp = getattr(module_name, temp_operation)
+                        if callable(temp):                                  # функция
                             self.op_st.insert(0, (temp, 4))
-                        else:                                   # константа
+                        else:                                               # константа
                             self.unary = False
                             self.eval_list.append(temp)
-                        egg = ''
+                        temp_operation = ''
                         break
                 else:
-                    if egg == '(':
+                    if temp_operation == '(':
                         self.unary = True
                         if self.implicit_mul is True:   # перед скобкой вставляем неявное умножение
                             self.make_note('*', True)
-                        self.op_st.insert(0, (egg, 0))
+                        self.op_st.insert(0, (temp_operation, 0))
 
-                    elif egg == ')':
+                    elif temp_operation == ')':
                         self.unary = False
                         for o in self.op_st:            # выгружаем все операции до открывающей скобки
                             if o[0] == '(':
@@ -145,43 +161,45 @@ class Calc:
                         self.implicit_mul = True        # после закрывающей скобки может быть неявное умножение
 
                     # константа
-                    elif egg in self.const:
+                    elif temp_operation in self.const:
                         self.unary = False
-                        self.eval_list.append(self.const[egg])
+                        if self.implicit_mul is True:   # перед скобкой вставляем неявное умножение
+                            self.make_note('*', True)
+                        self.eval_list.append(self.const[temp_operation])
                         # self.check_neg()
                         self.implicit_mul = True
 
                     # выбор из math
-                    elif egg in dir(math):
+                    elif temp_operation in dir(math):
                         if self.implicit_mul:
                             self.make_note('*', True)
-                        self.op_st.insert(0, (getattr(math, egg), 4))
+                        self.op_st.insert(0, (getattr(math, temp_operation), 4))
 
-                    elif egg in self.func:
-                        self.op_st.insert(0, self.func[egg])
+                    elif temp_operation in self.func:
+                        self.op_st.insert(0, self.func[temp_operation])
 
-                    elif egg in self.bin_op:
-                        if egg == '^' or (egg == '-' and self.unary is False):
+                    elif temp_operation in self.bin_op:
+                        if temp_operation == '^' or (temp_operation == '-' and self.unary is False):
                             self.unary = True
                         i = 0
-                        if self.op_st and not self.op_st[0][0] == self.bin_op[egg][0] == operator.pow:
+                        if self.op_st and not self.op_st[0][0] == self.bin_op[temp_operation][0] == operator.pow:
                             for o in self.op_st:          # выталкиваем приоритетные, префиксные операции
-                                if o[1] >= self.bin_op[egg][1]:
+                                if o[1] >= self.bin_op[temp_operation][1]:
                                     self.eval_list.append(o[0])
                                     del self.op_st[0]
                                 else:
                                     break
                                 i += 1
                         self.op_st[:i] = []
-                        self.op_st.insert(0, self.bin_op[egg])
+                        self.op_st.insert(0, self.bin_op[temp_operation])
                         self.implicit_mul = False       # наличие бинарной операции исключает неявное умножение
 
                     else:
-                        if self.lim <= len(egg):
+                        if self.lim <= len(temp_operation):
                             raise ValueError('unknown function or constant!')
                         continue
-                    egg = ''
-        if egg:
+                    temp_operation = ''
+        if temp_operation:
             raise ValueError('unknown function or constant!')
         if impl is True:    # обработка неявного умножения. Чисел нет, лист операций трогать нельзя - выход из функции
             self.implicit_mul = False
@@ -192,7 +210,7 @@ class Calc:
             self.eval_list.append(o[0])
         self.op_st = []
 
-    def check_num(self, temp_num: str):            # определение типа числа
+    def check_num(self, temp_num):            # определение типа числа
         if '.' in temp_num:
             if len(temp_num) == 1:
                 raise ValueError('incorrect using dots!')
@@ -201,6 +219,10 @@ class Calc:
             self.eval_list.append(int(temp_num))
 
     def eval_note(self):
+        """
+        Evaluate Reverse Polish Entry from self.evaluate_list
+        :return: int or float
+        """
         num_stack = []
         for i in self.eval_list:
             if not callable(i):                     # данный элемент не функция, т.е., число -> поместить в стек чисел
@@ -219,39 +241,38 @@ class Calc:
         return num_stack[0]
 
 
-def calculate():
-    re = {' + ': '+', ' * ': '*', ', ': ',', ' - ': '-', '\'': '', '"': ''}
+def calculate(expr, modules):
+    combinations_for_replace = {' + ': '+', ' * ': '*', ', ': ',', ' - ': '-', '\'': '', '"': ''}
+
+    for i, r in combinations_for_replace.items():
+        expr = expr.replace(i, r)
+    if ' ' in expr:
+        raise ValueError('spaces in expression!')
+    if not expr:
+        raise ValueError('empty expression!')
+    if expr.count('(') != expr.count(')'):
+        raise ValueError('brackets are not balanced!')
+    if '$' in expr or 'q' in expr:
+        raise ValueError('incorrect symbols!')
+    else:
+        expr = expr.replace('//', '$')
+    cd = Calc(modules)
+    print(cd.evaluate_expression(expr))
+
+
+def parse_cmd_args():
     try:
         parser = argparse.ArgumentParser(description='Pure-python command-line calculator.')
 
         parser.add_argument('EXPRESSION', help='Expression string to evaluate')
         parser.add_argument('-m', '--use-modules', action='store', nargs='*',
-                            dest='user', help='Using your own module', default=None)
-        pr = parser.parse_args().__dict__
-        s = pr['EXPRESSION']
+                            dest='modules', help='Using your own module', default=None)
+        pr = parser.parse_args()
+        calculate(pr.EXPRESSION, pr.modules)
 
-        user = []
-        if pr['user']:
-            user += pr['user']
-        for i, r in re.items():
-            s = s.replace(i, r)
-        if ' ' in s:
-            raise ValueError('spaces in expression!')
-        if not s:
-            raise ValueError('empty expression!')
-        if s.count('(') != s.count(')'):
-            raise ValueError('brackets are not balanced!')
-        if '$' in s or 'q' in s:
-            raise ValueError('incorrect symbols!')
-        else:
-            s = s.replace('//', '$')
-            s = s.replace('epi', 'q')
-            s = s.replace('pie', 'q')
-        cd = Calc(user)
-        print(cd.my_eval(s))
     except Exception as e:
         print('ERROR: ', e, sep='\n', end='')
 
 
 if __name__ == '__main__':
-    calculate()
+    parse_cmd_args()
