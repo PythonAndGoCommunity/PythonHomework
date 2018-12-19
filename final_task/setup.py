a6aa25ddb94026e0682980162e6ab66fb8f91af3
Pavel Kuzmich <45352360+pavelkuz99@users.noreply.github.com>
2018-12-19 05:08:55 +0300

    Update setup.py

--- final_task/setup.py
+++ final_task/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pycalc',
-    version='1.0',
+    version='1.0.1',
     author='Pavel Kuzmich',
     author_email='pavelkuz99@outlook.com',
     description='Pure Python command-line calculator',
