cabe3c55c18c86e5679a6cfe40b10567961a7849
Elena Volkova <volkovaelen87@gmail.com>
2018-12-29 14:19:20 +0300

    [core] Add setup.py

---
+++ final_task/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pycalc',
+    version='0.1',
+    author='Elena Volkova',
+    author_email='volkovaelen87@gmail.com',
+    description='Python command-line calculator',
+    packages=find_packages(),
+    scripts=['pycalc']
+)
